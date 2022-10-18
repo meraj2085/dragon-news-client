ee351a4e38967877eff01d85c65f570f1080e241
meraj2085 <samirmeraj60@gmail.com>
2022-10-18 11:04:39 +0600

    adding responsive side nav

--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaTwitter,
+  FaWhatsapp,
+  FaTwitch,
+  FaInstagram,
+} from "react-icons/fa";
+import ListGroup from "react-bootstrap/ListGroup";
+import BrandCarousel from "../BrandCarousel/BrandCarousel";
 
 const RightSideNav = () => {
-     return (
-          <div>
-               <h2>Right side nav</h2>
-          </div>
-     );
+  return (
+    <div>
+      <ButtonGroup vertical>
+        <Button variant="outline-primary">
+          <FaGoogle></FaGoogle> Login with google
+        </Button>{" "}
+        <Button variant="outline-dark" className="mt-2">
+          <FaGithub></FaGithub> Login with github
+        </Button>
+      </ButtonGroup>
+      <div className="mt-4">
+        <h5>Find us on</h5>
+        <ListGroup>
+          <ListGroup.Item className="mb-2">
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaWhatsapp /> Whatsapp
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitch /> Twitch
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaInstagram /> Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div>
+        <BrandCarousel></BrandCarousel>
+      </div>
+    </div>
+  );
 };
 
 export default RightSideNav;
